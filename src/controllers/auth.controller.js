074454d3abbbe4ff6548b7b1087ee01b6c5de8b2
0xlakhe <249052680+0xlakhe@users.noreply.github.com>
2026-02-19 17:28:51 +0545

    Complete Register User

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,49 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+async function registerUser(req, res) {
+  const { username, email, password, role = "user" } = req.body;
+
+  //checking if user exists
+  const userExists = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (userExists) {
+    res.status(409).json({
+      message: "User exists",
+    });
+  }
+
+  //hashing password
+  const hash = await bcrypt.hash(password, 10);
+
+  //creating new user
+  const user = await userModel.create({
+    username,
+    email,
+    password: hash,
+    role,
+  });
+
+  //creating token using user's id
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET,
+  );
+
+  //sending token to cookie with name token
+  res.cookie("token", token);
+
+  res.status(201).json({
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    },
+  });
+}
+
+module.exports = { registerUser };
